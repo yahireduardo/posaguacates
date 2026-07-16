e2041921e9955109767b5bcd5075e868309f6db6
yahireduardo <Yahireduardo@hotmail.com>
2026-07-16 00:09:29 -0600

    delete

--- pos-backend/routes/pagos.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pagosController = require('../controllers/pagosController');
-
-router.post('/pagar', pagosController.registrarPago);
-
-module.exports = router;
--- pos-backend/routes/ventas.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ventasController = require('../controllers/ventasController');
-
-router.post('/crear', ventasController.crearVenta);
-
-module.exports = router;
